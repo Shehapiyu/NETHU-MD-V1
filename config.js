8634d32a518a91aba61c61ef1c9bb26ea7a9e5d9
Shehapiyu <168281562+Shehapiyu@users.noreply.github.com>
2024-10-02 14:15:36 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,14 +5,14 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "put your session id ",
+SESSION_ID: process.env.SESSION_ID || "3HAmRDJA#QhpAwjGw8YsZ_oY4xKI7yb0RNOUN7tmh30BDqopWCaA",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://imgtr.ee/images/2024/09/17/37f9be869d1404ef1a5d188178450537.jpeg",
 ALIVE_MSG: process.env.ALIVE_MSG || "Hello I'm alive now",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
 SUDO_NB: process.env.SUDO_NB || "94704227534",
-OWNER_NAME: process.env.OWNER_NAME || "Nethmika MAIN",
-BOT_NAME: process.env.BOT_NAME || "NETHU-MD",
+OWNER_NAME: process.env.OWNER_NAME || "shehan MAIN",
+BOT_NAME: process.env.BOT_NAME || "sheha-MD",
 AUTO_VOICE: process.env.AUTO_VOICE || "true"
 
 };
